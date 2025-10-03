b4a8cf7d66a8a1c0ff7d1f51b8225c74fc032b7e
Aaromance <abdulrahmanahmedjalingo@gmail.com>
2025-10-03 08:49:09 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const settings = {
   packname: 'Knight Bot',
   author: '‎',
   botName: "Knight Bot",
-  botOwner: '𖣘Goldprince☆⁩', // Your name
+  botOwner: 'Goldprince☆⁩', // Your name
   ownerNumber: '+2348160219073', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
